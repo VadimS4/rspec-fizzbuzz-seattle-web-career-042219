60d2519742b410cce6f6ba52b2d663c170dcc45f
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-10 03:56:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,11 +6,11 @@ def fizzbuzz(num)
   elsif num % 5 == 0
     return "Buzz"
   
-  elsif num % 3 == 0 && num % 5 == 0
+  elsif num % 3 && num % 5 == 0
     return "FizzBuzz"
     
   else
-    return " "
+    return "nil"
     
   end
 end
